0192860822b7dc6b933a2a982aff9bf3fef0fe30
stkd-io <105894637+stkd-io@users.noreply.github.com>
2022-12-22 08:39:22 -0800

    Create benchmark.sh

---
+++ benchmark.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Import the security@parity.io GPG key
+gpg --recv-keys --keyserver hkps://keys.mailvelope.com 9D4B2B6EB8F97156D19669A9FF0812D491B96798
+gpg --export 9D4B2B6EB8F97156D19669A9FF0812D491B96798 > /usr/share/keyrings/parity.gpg
+# Add the Parity repository and update the package index
+echo 'deb [signed-by=/usr/share/keyrings/parity.gpg] https://releases.parity.io/deb release main' > /etc/apt/sources.list.d/parity.list
+apt update
+# Install the `parity-keyring` package - This will ensure the GPG key
+# used by APT remains up-to-date
+apt install parity-keyring
+# Install polkadot
+apt install polkadot
+
+polkadot benchmark machine
